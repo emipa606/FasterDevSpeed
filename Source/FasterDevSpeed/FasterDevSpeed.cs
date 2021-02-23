28ff87c30793d513bd5354da81395bde49b5d81c
emipa606 <emil.offermo@litet.net>
2021-02-23 19:38:32 +0100

    Updated to 1.2

--- Source/FasterDevSpeed/FasterDevSpeed.cs
+++ Source/FasterDevSpeed/FasterDevSpeed.cs
@@ -2,49 +2,45 @@
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using Harmony;
+using HarmonyLib;
+using Verse;
 
 namespace FasterDevSpeed
 {
-    [Verse.StaticConstructorOnStartup]
-    static class FasterDevSpeed
+    [StaticConstructorOnStartup]
+    internal static class FasterDevSpeed
     {
-
         static FasterDevSpeed()
         {
-            Verse.Log.Message("FasterDevSpeed starting up");
+            Log.Message("FasterDevSpeed starting up");
 
-            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.sparr.fasterdevspeed");
+            var harmony = new Harmony("rimworld.sparr.fasterdevspeed");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
-
-
     }
 
-    [HarmonyPatch(typeof(Verse.TickManager))]
+    [HarmonyPatch(typeof(TickManager))]
     [HarmonyPatch("get_TickRateMultiplier")]
-    static class Patch_get_TickRateMultiplier
+    internal static class Patch_get_TickRateMultiplier
     {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var codes = new List<CodeInstruction>(instructions);
             int i;
             for (i = 5; i < codes.Count; i++)
             {
-                if (
-                    codes[i].opcode == OpCodes.Ret &&
-                    (float)codes[i-1].operand == -1f && // found `return -1f`
-                    codes[i-5].opcode == OpCodes.Ldc_R4 &&
-                    codes[i-4].opcode == OpCodes.Ret &&
-                    codes[i-3].opcode == OpCodes.Ldc_R4 &&
-                    codes[i-2].opcode == OpCodes.Ret // hopefully found the end of `case TimeSpeed.Ultrafast:`
-                    )
+                if (codes[i].opcode != OpCodes.Ret || (float) codes[i - 1].operand != -1f ||
+                    codes[i - 5].opcode != OpCodes.Ldc_R4 || codes[i - 4].opcode != OpCodes.Ret ||
+                    codes[i - 3].opcode != OpCodes.Ldc_R4 || codes[i - 2].opcode != OpCodes.Ret)
                 {
-                    codes[i-5].operand = (float)codes[i - 5].operand * 4;
-                    codes[i-3].operand = (float)codes[i - 3].operand * 4;
-                    break;
+                    continue;
                 }
+
+                codes[i - 5].operand = (float) codes[i - 5].operand * 4;
+                codes[i - 3].operand = (float) codes[i - 3].operand * 4;
+                break;
             }
+
             return codes.AsEnumerable();
         }
     }
