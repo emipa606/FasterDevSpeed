21fbfaebed9848dbe60554a248cf0df73f5ed62b
emipa606 <emil.offermo@litet.net>
2021-07-09 14:55:04 +0200

    Mod updated for 1.3 and passed autotests

--- Source/FasterDevSpeed/FasterDevSpeed.cs
+++ Source/FasterDevSpeed/FasterDevSpeed.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
+﻿using System.Reflection;
 using HarmonyLib;
 using Verse;
 
@@ -16,32 +13,6 @@ namespace FasterDevSpeed
 
             var harmony = new Harmony("rimworld.sparr.fasterdevspeed");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-        }
-    }
-
-    [HarmonyPatch(typeof(TickManager))]
-    [HarmonyPatch("get_TickRateMultiplier")]
-    internal static class Patch_get_TickRateMultiplier
-    {
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var codes = new List<CodeInstruction>(instructions);
-            int i;
-            for (i = 5; i < codes.Count; i++)
-            {
-                if (codes[i].opcode != OpCodes.Ret || (float) codes[i - 1].operand != -1f ||
-                    codes[i - 5].opcode != OpCodes.Ldc_R4 || codes[i - 4].opcode != OpCodes.Ret ||
-                    codes[i - 3].opcode != OpCodes.Ldc_R4 || codes[i - 2].opcode != OpCodes.Ret)
-                {
-                    continue;
-                }
-
-                codes[i - 5].operand = (float) codes[i - 5].operand * 4;
-                codes[i - 3].operand = (float) codes[i - 3].operand * 4;
-                break;
-            }
-
-            return codes.AsEnumerable();
         }
     }
 }
---
+++ Source/FasterDevSpeed/Patch_get_TickRateMultiplier.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Verse;
+
+namespace FasterDevSpeed
+{
+    [HarmonyPatch(typeof(TickManager))]
+    [HarmonyPatch("get_TickRateMultiplier")]
+    internal static class Patch_get_TickRateMultiplier
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            int i;
+            for (i = 5; i < codes.Count; i++)
+            {
+                if (codes[i].opcode != OpCodes.Ret || (float) codes[i - 1].operand != -1f ||
+                    codes[i - 5].opcode != OpCodes.Ldc_R4 || codes[i - 4].opcode != OpCodes.Ret ||
+                    codes[i - 3].opcode != OpCodes.Ldc_R4 || codes[i - 2].opcode != OpCodes.Ret)
+                {
+                    continue;
+                }
+
+                codes[i - 5].operand = (float) codes[i - 5].operand * 4;
+                codes[i - 3].operand = (float) codes[i - 3].operand * 4;
+                break;
+            }
+
+            return codes.AsEnumerable();
+        }
+    }
+}
